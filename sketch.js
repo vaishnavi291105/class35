308a8b616fdd2b4ac2813d3c0a443471acab36c1
vaishnavi291105 <78863218+vaishnavi291105@users.noreply.github.com>
2021-06-13 10:30:25 +0545

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var canvas , brackgroundImage;
+var gameState = 0;
+var playerCount , database;
+var form , player, game;
+
+
+function setup(){
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  
+  
+}
+
